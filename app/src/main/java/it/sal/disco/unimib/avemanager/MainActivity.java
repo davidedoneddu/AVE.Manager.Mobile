e99d8c545fd53570f876c4e60e19407dba356877
davidedoneddu <davide.doneddu02@gmail.com>
2025-02-24 21:48:40 +0100

    Create File Project

---
+++ app/src/main/java/it/sal/disco/unimib/avemanager/MainActivity.java
@@ -0,0 +1,14 @@
+package it.sal.disco.unimib.avemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
